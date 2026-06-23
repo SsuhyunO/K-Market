8d75fbc04b2b53325f5e79469889f681f92203b5
jeomseon0516 <126733377+jeomseon0516@users.noreply.github.com>
2026-06-23 17:49:30 +0900

    modified MainController GetMapping

--- src/main/java/org/example/k_market/controller/admin/MainController.java
+++ src/main/java/org/example/k_market/controller/admin/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-    @GetMapping({"/admin/main", "/"})
+    @GetMapping({"/admin/main", "/admin"})
     public String main() {
         return "admin/main";
     }
