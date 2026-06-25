709fee79644a83e8db17de4bc2287f8816b2369b
spirometry <spirometry@naver.com>
2026-06-25 18:07:57 +0900

    cs.index

---
+++ src/main/java/org/example/k_market/controller/CsController.java
@@ -0,0 +1,13 @@
+package org.example.k_market.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CsController {
+    @GetMapping("/cs/index")
+    public String index(){
+        return "/cs/index";
+    }
+
+}
