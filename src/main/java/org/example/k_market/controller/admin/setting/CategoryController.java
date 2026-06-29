9c045301e19b4eae3bf2e7a5b75d741bf34d0e06
jeomseon0516 <126733377+jeomseon0516@users.noreply.github.com>
2026-06-30 01:02:34 +0900

    product management

---
+++ src/main/java/org/example/k_market/controller/admin/setting/CategoryController.java
@@ -0,0 +1,13 @@
+package org.example.k_market.controller.admin.setting;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CategoryController {
+
+    @GetMapping("/admin/category-management")
+    public String category() {
+        return "admin/setting/category";
+    }
+}
