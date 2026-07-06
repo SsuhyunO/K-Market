8be1170b4cc2273c58c9eace83885a39b76246eb
jeomseon0516 <126733377+jeomseon0516@users.noreply.github.com>
2026-07-06 14:05:51 +0900

    modified repository

--- src/main/java/org/example/k_market/repository/product/ProductOptionItemRepository.java
+++ src/main/java/org/example/k_market/repository/product/ProductOptionItemRepository.java
@@ -1,8 +1,9 @@
 package org.example.k_market.repository.product;
 
+import org.example.k_market.entity.product.ProductOptionItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductOptionItemRepository extends JpaRepository<ProductOptionItemRepository, Integer> {
+public interface ProductOptionItemRepository extends JpaRepository<ProductOptionItem, Integer> {
 }
--- src/main/java/org/example/k_market/repository/product/ProductVariantItemRepository.java
+++ src/main/java/org/example/k_market/repository/product/ProductVariantItemRepository.java
@@ -1,8 +1,9 @@
 package org.example.k_market.repository.product;
 
+import org.example.k_market.entity.product.ProductVariantItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductVariantItemRepository extends JpaRepository<ProductVariantItemRepository, Integer> {
+public interface ProductVariantItemRepository extends JpaRepository<ProductVariantItem, Integer> {
 }
